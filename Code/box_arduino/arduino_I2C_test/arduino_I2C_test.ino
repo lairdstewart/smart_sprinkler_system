3b66236a0ae5c29692e42a6bdcc65b4b3349c455
Mehul Gupta <mgupta320@gatech.edu>
2023-04-07 15:37:02 -0400

    i2c trial code with dummy data

--- Code/box_arduino/arduino_I2C_test/arduino_I2C_test.ino
+++ Code/box_arduino/arduino_I2C_test/arduino_I2C_test.ino
@@ -1,9 +1,27 @@
+//following Controller Reader from: https://docs.arduino.cc/learn/communication/wire
+#include <Wire.h>
+
 void setup() {
   // put your setup code here, to run once:
-
+  Wire.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  String dataString = "";
+  float voltage = 0.0F;
 
+  Wire.requestFrom(8, 7);
+  while (Wire.available())
+  {
+    char c = Wire.read();
+    dataString = dataString + c;
+    Serial.print(c);
+  }
+  
+  voltage = dataString.toFloat();
+  Serial.println(voltage);
+
+  delay(1000);
 }
--- Code/box_feather/feather_I2C_test/feather_I2C_test.ino
+++ Code/box_feather/feather_I2C_test/feather_I2C_test.ino
@@ -1,9 +1,26 @@
+//following Peripheral Sender from: https://docs.arduino.cc/learn/communication/wire
+#include <Wire.h>
+#include <avr/dtostrf.h>
+
+float voltage = 0.0F;
+char voltBuff[7];
+
 void setup() {
   // put your setup code here, to run once:
-
+  Wire.begin(8);
+  Wire.onRequest(requestEvent);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //simulate geting data from sensors
+  voltage = voltage + 0.0001F;
+  if (voltage >= 100)
+    voltage = 0.0F;
+  dtostrf(voltage,-7,5, voltBuff); //7 byte maximum
+  delay(100);
+}
 
+void requestEvent() {
+  Wire.write(voltBuff);
 }
